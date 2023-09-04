c93fddf885d1a4c254dfaf1aa526cf7305de2121
wladyrjusta <wladyr.justa@gmail.com>
2023-09-04 14:53:27 -0300

    Implementados testes para a class Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("queijo parmesão")
+    ingredient_2 = Ingredient("queijo parmesão")
+    assert ingredient_1.__hash__() == ingredient_2.__hash__()
+    assert ingredient_1.__eq__(ingredient_2) is not False
+    assert ingredient_1.__repr__() == "Ingredient('queijo parmesão')"
+
+    different_ingredient_1 = Ingredient("queijo parmesão")
+    different_ingredient_2 = Ingredient("presunto")
+    assert (
+        different_ingredient_1.__hash__() != different_ingredient_2.__hash__()
+    )
+    assert different_ingredient_1.__eq__(different_ingredient_2) is not True
+    assert different_ingredient_1.name == "queijo parmesão"
+    assert (
+        different_ingredient_1.restrictions == {
+            Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+        }
+    )
